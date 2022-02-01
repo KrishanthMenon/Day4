4e3ce1f024220e7bf9e136a00b446fc2ad45c618
Krishanth M <krishanthmenon88@gmail.com>
2022-02-01 10:28:01 +0530

    commit

--- src/com/day4/MyEncapsulation.java
+++ src/com/day4/MyEncapsulation.java
@@ -1,5 +1,41 @@
 package com.day4;
+class Employee
+{
+	public Employee(int empID, String name, float salary) {
+		this.empID = empID;
+		this.name = name;
+		this.salary = salary;
+	}
+	public int getEmpID() {
+		return empID;
+	}
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	private int empID;
+	private String name;
+	private float salary;
+	
+}
 
 public class MyEncapsulation {
-
+	public static void main(String args[])
+	{
+		Employee employee=new Employee(1331, "Mr.X", 15000);
+		System.out.println(employee.getEmpID());
+		System.out.println(employee.getName());
+		System.out.println(employee.getSalary());
+	}
 }
